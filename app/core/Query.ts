92317250b0f82e049935c6e0c0114b25cb3469b4
Jagger <634750802@qq.com>
2022-03-03 12:04:04 +0800

    feat: gen schema

--- app/core/Query.ts
+++ app/core/Query.ts
@@ -2,16 +2,9 @@ import {mkdir, readFile} from 'fs/promises'
 import path from 'path'
 import Cache, {CachedData} from "./Cache";
 import {DateTime} from "luxon";
+import type { ParamsSchema } from '../../params.schema'
 
-export interface QueryParams {
-  cacheHours: number,
-  params: {
-    name: string,
-    replaces: string,
-    template?: Record<string, string>,
-    default?: string
-  }[]
-}
+export type QueryParams = ParamsSchema
 
 export class BadParamsError extends Error {
   readonly msg: string
