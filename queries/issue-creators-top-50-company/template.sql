134d4dd911910af9037c9763f18e9009022e7b95
Mini256 <minianter@foxmail.com>
2022-03-24 21:58:09 +0800

    fix: fix typo

--- queries/issue-creators-top-50-company/template.sql
+++ queries/issue-creators-top-50-company/template.sql
@@ -2,7 +2,7 @@ select *
 from (
      select
          trim(replace(replace(replace(replace(replace(replace(replace(replace(lower(u.company), ',', ''), '-', ''), '@', ''), 'www.', ''), 'inc', ''), '.com', ''), '.cn', ''), '.', '')) as company_name,
-         count(distinct github_events.actor_id) as code_contributors
+         count(distinct github_events.actor_id) as issue_creators
      from github_events
               use index(index_github_events_on_repo_id)
           left join users u ON github_events.actor_login = u.login
@@ -10,5 +10,5 @@ from (
      group by 1
 ) sub
 where length(company_name) != 0 and company_name not in ('-', 'none', 'no', 'home', 'n/a', 'null')
-order by code_contributors desc
+order by issue_creators desc
 limit 50;
