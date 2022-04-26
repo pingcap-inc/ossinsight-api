2fcd290ea681742686ce350bbbc689dca3106850
Mini256 <minianter@foxmail.com>
2022-04-26 15:16:54 +0800

    feat: add querys

---
+++ queries/contributors-top-50-company/template.sql
@@ -0,0 +1,14 @@
+select *
+from (
+    select
+        trim(replace(replace(replace(replace(replace(replace(replace(replace(u.company, ',', ''), '-', ''), '@', ''), '.', ''), 'ltd', ''), 'inc', ''), 'com', ''), 'www', '')) as company_name,
+        count(distinct actor_id) as stargazers
+    from github_events
+    use index(index_github_events_on_repo_id)
+    left join users u ON github_events.actor_login = u.login
+    where repo_id in (41986369) and github_events.type IN ('IssuesEvent', 'PullRequestEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent')
+    group by 1
+ ) sub
+where length(company_name) != 0 and company_name not in ('-', '--- click here ---', 'none', 'no', 'home', 'n/a')
+order by stargazers desc
+limit 50;
