4b84e3ac0284e85d7bdd2c4710563c80494af64b
Mini256 <minianter@foxmail.com>
2022-04-26 22:13:25 +0800

    feat: add more queries

--- queries/analyze-loc-per-month/template.sql
+++ queries/analyze-loc-per-month/template.sql
@@ -2,12 +2,14 @@ select
     event_month,
     sum(additions) as additions,
     sum(deletions) as deletions,
-    sum(additions) - sum(deletions) as changes
+    sum(additions) - sum(deletions) as net_additions,
+    sum(additions) + sum(deletions) as changes
 from github_events
 use index(index_github_events_on_repo_id)
 where
     repo_id = 41986369
     and type = 'PullRequestEvent'
+    and action = 'closed'
     and pr_merged = true
 group by event_month
 order by event_month
