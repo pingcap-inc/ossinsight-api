3782ed2baae4d4c32b0fbf742c9bd86046fb1067
Mini256 <minianter@foxmail.com>
2022-05-31 14:07:06 +0800

    feat: add timestamp

--- queries/events-increment/template.sql
+++ queries/events-increment/template.sql
@@ -1,6 +1,7 @@
 SELECT
     COUNT(*) AS cnt,
-    max(created_at) AS latest_created_at
+    MAX(created_at) AS latest_created_at,
+    UNIX_TIMESTAMP(MAX(created_at)) AS latest_timestamp
 FROM github_events
 WHERE
     created_at BETWEEN FROM_UNIXTIME(1653944647) AND (UTC_TIMESTAMP - INTERVAL 5 MINUTE)
