e710d4d1e2367c47992e7186044ff7bd7aacf340
Mini256 <minianter@foxmail.com>
2022-06-20 08:17:19 +0800

    fix: only calc bot contribution

--- queries/bot-contribution-ranking/template.sql
+++ queries/bot-contribution-ranking/template.sql
@@ -4,7 +4,7 @@ SELECT
 FROM github_events ge
 WHERE
     type IN ('PullRequestEvent', 'IssuesEvent', 'PullRequestReviewEvent', 'PushEvent')
-    AND actor_login IS NOT NULL
+    AND actor_login REGEXP '^(bot-.+|.+bot|.+\\[bot\\]|.+-bot-.+|robot-.+|.+-ci-.+|.+-ci|.+-testing|.+clabot.+|.+-gerrit|k8s-.+|.+-machine|.+-automation|github-.+|.+-github|.+-service|.+-builds|codecov-.+|.+teamcity.+|jenkins-.+|.+-jira-.+)$'
     AND (
         (type = 'PullRequestEvent' AND action = 'opened') OR
         (type = 'IssuesEvent' AND action = 'opened') OR  
